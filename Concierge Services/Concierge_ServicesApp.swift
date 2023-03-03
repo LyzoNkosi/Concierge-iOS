d896b2a5bcfc9bb0ad4b0f933f005eedf28fa58b
norrinraddtech <terrence.chipuka@norrinradd.co>
2023-03-03 11:01:29 +0200

    Initial Commit

---
+++ Concierge Services/Concierge_ServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concierge_ServicesApp.swift
+//  Concierge Services
+//
+//  Created by Mac on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct Concierge_ServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
