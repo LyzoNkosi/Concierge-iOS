9fd2547aa85d2829ac43bd3ec1f46d6673ac5880
vanlee007 <VanLee.Chiqwada@norrinradd.co>
2023-03-06 15:13:15 +0200

    Added task view

--- Concierge Services/TaskDetailView.swift
+++ Concierge Services/TaskDetailView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TaskDetailView: View {
     var body: some View {
         NavigationView{
-            VStack{
+            VStack (alignment: .leading, spacing: 8){
                 
                 HStack {
                     Image(systemName: "car").frame(width: 24, height: 24)
@@ -18,12 +18,67 @@ struct TaskDetailView: View {
                         .font(.title2)
                         .multilineTextAlignment(.leading)
                 }
+                .padding()
+                
+                VStack(alignment: .leading, spacing: 8){
+                    Label {
+                        Text("14:04")
+                            .font(.subheadline)
+                    } icon: {
+                        Image(systemName: "calendar.badge.clock")
+                    }
+                    .padding()
+                    
+                    Label {
+                        Text("Boarding Pass")
+                            .font(.subheadline)
+                    } icon: {
+                        Image(systemName: "square.and.arrow.down")
+                    }
+                    .padding()
+                    
+                }
+                
+                
                 
             }
             .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
             .padding()
             .navigationTitle("Task Details")
-        }
+            
+            .safeAreaInset(edge: .bottom) {
+                HStack(alignment: .center, spacing: 8){
+                    EditTaskButtonContent()
+                    CancelTaskButtonContent()
+                }
+                
+            } .padding(12)
+            
+        }.navigationTitle("Task Detials")
+    }
+}
+
+struct EditTaskButtonContent : View {
+    var body: some View {
+        return Text("Modify")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 160, height: 60)
+            .background(primaryBlack)
+            .cornerRadius(15.0)
+    }
+}
+
+struct CancelTaskButtonContent : View {
+    var body: some View {
+        return Text("Cancel")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 160, height: 60)
+            .background(primaryBlack)
+            .cornerRadius(15.0)
     }
 }
 
