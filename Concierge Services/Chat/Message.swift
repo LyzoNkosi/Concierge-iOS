d5b6197fe3c905f3b0ecaa1bd3ffb6d10e37400f
vanlee007 <VanLee.Chiqwada@norrinradd.co>
2023-03-07 11:28:52 +0200

    Test Changes

--- Concierge Services/Chat/Message.swift
+++ Concierge Services/Chat/Message.swift
@@ -9,11 +9,11 @@ struct DataSource {
     static let firstUser = User(name: "Mbali", avatar: "profile")
     static var secondUser = User(name: "Cool Client", avatar: "profile", isCurrentUser: true)
     static let messages = [
-        Message(content: "Hi, I need a trip to Vegas", user: DataSource.firstUser),
+        Message(content: "Hi, I need a trip to Vegas.", user: DataSource.firstUser),
         Message(content: "How many people will you be travelling with?", user: DataSource.secondUser),
         Message(content: "Just me. I need a break from the world.", user: DataSource.firstUser),
-        Message(content: "I have made the arrangements for you", user: DataSource.firstUser),
-        Message(content: "Great, let me check", user: DataSource.secondUser),
-        Message(content: "Cool cool", user: DataSource.firstUser)
+        Message(content: "I have made the arrangements for you.", user: DataSource.firstUser),
+        Message(content: "Great, let me check...", user: DataSource.secondUser),
+        Message(content: "Cool cool.", user: DataSource.firstUser)
     ]
 }
