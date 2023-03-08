4148f695d083cd3b5a2ef4694a2fa56df2e0cbb7
vanlee007 <VanLee.Chiqwada@norrinradd.co>
2023-03-08 16:15:14 +0200

    Working on Auth

--- Concierge Services/Concierge_ServicesApp.swift
+++ Concierge Services/Concierge_ServicesApp.swift
@@ -1,8 +1,35 @@
 import SwiftUI
+import Firebase
 
+/*@main
+struct Concierge_ServicesApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}*/
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    // MARK: - Methods
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
 @main
 struct Concierge_ServicesApp: App {
     
+    // MARK: - Properties
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+    // MARK: - Life Cycle
+    init() {
+        FirebaseApp.configure()
+    }
+  
+    // MARK: - UI Elements
     var body: some Scene {
         WindowGroup {
             ContentView()
